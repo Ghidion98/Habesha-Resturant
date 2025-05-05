0749217039ada76d26b327bd8e9d882dfc70a481
Man_17 <naniluis282@gmail.com>
2025-05-05 11:19:45 -0400

    Updated

--- scripts/app.js
+++ scripts/app.js
@@ -9,3 +9,12 @@ function toggleMenu() {
   }
   menuOpen = !menuOpen;
 }
+
+const form = document.querySelector("form");
+  const message = document.getElementById("message");
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault(); // Stop the page from refreshing
+    message.style.display = "block"; // Show the success message
+    form.reset(); // Optional: clear form fields
+  });
